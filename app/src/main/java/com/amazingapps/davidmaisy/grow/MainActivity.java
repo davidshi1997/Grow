04a1c6b9591387ea4ba5ffec566fae2ac43aa7bc
davidshi1997 <davidshi1997@gmail.com>
2017-12-06 16:37:17 -0500

    added stylish buttons

--- app/src/main/java/com/amazingapps/davidmaisy/grow/MainActivity.java
+++ app/src/main/java/com/amazingapps/davidmaisy/grow/MainActivity.java
@@ -5,22 +5,27 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     // TODO: Should load in saved data, such as coins, seeds, and drinks
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences settings = getSharedPreferences("settings", MODE_PRIVATE);
-        if (!settings.contains("weight")) {
-            try {
-                Intent k = new Intent(MainActivity.this, IntroActivity.class);
-                startActivity(k);
-                finish();
-            } catch (Exception e) {
-            }
-        }
+//        SharedPreferences settings = getSharedPreferences("settings", MODE_PRIVATE);
+//        if (!settings.contains("dailywater")) {
+//            try {
+//                Intent k = new Intent(MainActivity.this, IntroActivity.class);
+//                startActivity(k);
+//                finish();
+//            } catch (Exception e) {
+//            }
+//        }
 
         setContentView(R.layout.activity_main);
     }
@@ -29,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        Date currentTime = Calendar.getInstance().getTime();
     }
 
     // TODO: Should save current waterlevel and seedtype before destruction as well as starts background notification service
